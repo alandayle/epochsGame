32743be22b5e45400ddd6c0c16c2f37e4c574367
Art Alandayle <180009514+alandayle@users.noreply.github.com>
2024-11-07 09:07:25 +0800

    Add loadingScreen class
    LoadingScreen will handle the loading of elements(Textures, fonts, audio, etc.) while the program waits to continue.

--- core/src/main/java/com/ArtManlangit/epochsGame/EpochsGame.java
+++ core/src/main/java/com/ArtManlangit/epochsGame/EpochsGame.java
@@ -3,13 +3,20 @@ package com.ArtManlangit.epochsGame;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class EpochsGame extends Game {
+    //screen settings
+    final float worldHeight = 800;
+    final float worldWidth = 450;
+    Viewport viewport;
+    Camera camera;
 
     @Override
     public void create() {
