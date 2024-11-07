dc332ceb39c6e379abf462488df785ee55074aef
Art Alandayle <180009514+alandayle@users.noreply.github.com>
2024-11-07 23:58:03 +0800

    Add main assetmanager, initital textures
    Created assetmanager to load assets before starting the game. Added some initial graphic files for creating textures and base game.

--- core/src/main/java/com/ArtManlangit/epochsGame/EpochsGame.java
+++ core/src/main/java/com/ArtManlangit/epochsGame/EpochsGame.java
@@ -3,23 +3,49 @@ package com.ArtManlangit.epochsGame;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FillViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class EpochsGame extends Game {
+
     //screen settings
     final float worldHeight = 800;
     final float worldWidth = 450;
     Viewport viewport;
     Camera camera;
 
+    //Loading screen and asset manager
+    AssetManager assetManager;
+    LoadingScreen loadingScreen;
+
     @Override
     public void create() {
+        //set camera and viewport
+        camera = new OrthographicCamera();
+        camera.position.set(worldWidth / 2, worldHeight / 2, 0);
+        viewport = new FillViewport(worldWidth, worldHeight, camera);
+
+        //setup loading screen and load all the assets
+        loadingScreen = new LoadingScreen(this);
+        assetManager = new AssetManager();
+        setScreen(loadingScreen);
+
+        //debug
+        debug();
+    }
+
+    //debug mode
+    public void debug() {
 
     }
+
+
 }
--- core/src/main/java/com/ArtManlangit/epochsGame/LoadingScreen.java
+++ core/src/main/java/com/ArtManlangit/epochsGame/LoadingScreen.java
@@ -1,16 +1,25 @@
 package com.ArtManlangit.epochsGame;
 
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.assets.AssetManager;
 
 public class LoadingScreen implements Screen {
+    EpochsGame epochsGame;
+
+    public LoadingScreen(EpochsGame epochsGame) {
+        this.epochsGame = epochsGame;
+    }
+
     @Override
     public void show() {
-        
+
     }
 
     @Override
     public void render(float delta) {
-
+        if (epochsGame.assetManager.update()) {
+            System.out.println("nice");
+        }
     }
 
     @Override
