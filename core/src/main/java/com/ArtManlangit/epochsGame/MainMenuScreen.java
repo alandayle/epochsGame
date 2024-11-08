70f74403c7451602a04c4ae55bcddb5ede0a4c6d
Art Alandayle <180009514+alandayle@users.noreply.github.com>
2024-11-08 19:05:42 +0800

    Add fading transition to SplashScreen & MainMenuScreen
    Fade in fade out effects added to both screens, with the timing synchronizing with the music's background.

--- core/src/main/java/com/ArtManlangit/epochsGame/MainMenuScreen.java
+++ core/src/main/java/com/ArtManlangit/epochsGame/MainMenuScreen.java
@@ -1,8 +1,66 @@
 package com.ArtManlangit.epochsGame;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class MainMenuScreen implements Screen {
+    //access to epochsGame and the main assetManager
+    EpochsGame epochsGame;
+    AssetManager assetManager;
+
+    //viewport and camera
+    Viewport viewport;
+    Camera camera;
+
+    //textures for assets in the loading screen
+    TextureAtlas mainBgsLogos;
+    TextureRegion currentBackground;
+
+    //Audio and sound effects
+    Music backgroundMusic;
+
+    //drawer
+    SpriteBatch batch;
+
+    //color value for transition
+    float colorValue;
+
+    //timer for transitions
+    float timer;
+
+
+    //constructor
+    public MainMenuScreen(EpochsGame epochsGame) {
+        //initialize variables
+        this.epochsGame = epochsGame;
+        this.assetManager = epochsGame.assetManager;
+        viewport = epochsGame.viewport;
+        camera = epochsGame.camera;
+        batch = epochsGame.batch;
+
+        //set colorTransition value to 1 at the start
+        colorValue = 1;
+
+        //set timer to 0
+        timer = 0;
+
+        //load and setup assets
+        loadAssets();
+    }
+
+    public void loadAssets() {
+        mainBgsLogos = assetManager.get("packedTextures/mainBgsLogos.atlas", TextureAtlas.class);
+        currentBackground = mainBgsLogos.findRegion("mainBackground(portrait)");
+    }
 
     @Override
     public void show() {
@@ -11,12 +69,63 @@ public class MainMenuScreen implements Screen {
 
     @Override
     public void render(float delta) {
+        input();
+        logic(delta);
+        draw();
+    }
+
+    public void input() {
+        if (Gdx.input.isTouched()) {
+            epochsGame.splashScreen.backgroundMusic.stop();
+        }
+    }
+
+    public void logic(float delta) {
+        timer += delta;
+
+        //fade in fade out logic
+        if (timer > 2) {
+            if (timer - 2 < 0.5) {
+                colorValue -= delta;
+            } else if (timer - 2 <= 1) {
+                colorValue += delta;
+            } else if (timer > 3) {
+                timer = 2;
+                colorValue = 1F;
+            }
+        }
+
+        //limit colorvalue to 0 - 1;
+        if (colorValue <= 0) {
+            colorValue = 0;
+        }
+        if (colorValue >= 1) {
+            colorValue = 1;
+        }
+    }
+
+    public void draw() {
+        ScreenUtils.clear(Color.BLACK);
+        camera.update();
+        viewport.apply();
+        batch.setProjectionMatrix(camera.combined);
+
+        //sets colorvalue before drawing
+        batch.setColor(colorValue, colorValue, colorValue, 1);
+
+        //begin draw
+        batch.begin();
+        batch.draw(currentBackground, 0, 0, epochsGame.worldWidth, epochsGame.worldHeight);
+        batch.end();
+
+        //reset color value
+        batch.setColor(1, 1,1, 1);
 
     }
 
     @Override
     public void resize(int width, int height) {
-
+        viewport.update(width, height, true);
     }
 
     @Override
--- core/src/main/java/com/ArtManlangit/epochsGame/SplashScreen.java
+++ core/src/main/java/com/ArtManlangit/epochsGame/SplashScreen.java
@@ -1,9 +1,12 @@
 package com.ArtManlangit.epochsGame;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
@@ -22,9 +25,22 @@ public class SplashScreen implements Screen {
     //textures for assets in the loading screen
     TextureAtlas mainBgsLogos;
     TextureRegion loadingLogo;
+    TextureRegion wearHeadphones;
+    TextureRegion mainMenu;
+    TextureRegion currentBackground;
+
+    //Audio and sound effects
+    Music backgroundMusic;
 
     //drawer
     SpriteBatch batch;
+
+    //color value for transition
+    float colorValue;
+
+
+    //timer for transitions
+    float timer;
 
 
     //constructor
@@ -36,31 +52,83 @@ public class SplashScreen implements Screen {
         camera = epochsGame.camera;
         batch = epochsGame.batch;
 
+        //set colorTransition value to 1 at the start
+        colorValue = 1;
+
+        //set timer to 0
+        timer = 0;
+
         //load and setup assets
         loadAssets();
     }
     public void loadAssets() {
+        //load textures
         mainBgsLogos = assetManager.get("packedTextures/mainBgsLogos.atlas", TextureAtlas.class);
         loadingLogo = mainBgsLogos.findRegion("logo(portrait)");
+        wearHeadphones = mainBgsLogos.findRegion("wearHeadphones(portrait)");
+        mainMenu = mainBgsLogos.findRegion("mainBackground(portrait)");
+
+        //set initial background
+        currentBackground = loadingLogo;
+
+        //load and configure music
+        backgroundMusic = assetManager.get("audio/background.mp3");
+        backgroundMusic.setLooping(true);
     }
 
     @Override
     public void show() {
-
+        backgroundMusic.play();
     }
 
     @Override
     public void render(float delta) {
         input();
-        logic();
+        logic(delta);
         draw();
     }
 
     public void input() {
-
+        if (timer > 8) {
+            epochsGame.mainMenuScreen = new MainMenuScreen(epochsGame);
+            epochsGame.setScreen(epochsGame.mainMenuScreen);
+        }
     }
 
-    public void logic() {
+    public void logic(float delta) {
+        //update timer to keep track of time passed
+        timer += delta;
+
+        //transition logic
+        if (timer <= 2) {
+            if (timer > 1) {
+                colorValue -= delta;
+            }
+        } else if (timer <= 6) {
+            if (currentBackground != wearHeadphones) {
+                currentBackground = wearHeadphones;
+            }
+            colorValue += delta;
+        } else if (timer <= 7) {
+            colorValue -= delta;
+        } else if (timer <= 8) {
+            if (currentBackground != mainMenu) {
+                currentBackground = mainMenu;
+            }
+            colorValue += delta;
+        }
+
+
+
+
+
+        //limit colorvalue to 0 - 1;
+        if (colorValue <= 0) {
+            colorValue = 0;
+        }
+        if (colorValue >= 1) {
+            colorValue = 1;
+        }
 
     }
 
@@ -70,10 +138,17 @@ public class SplashScreen implements Screen {
         viewport.apply();
         batch.setProjectionMatrix(camera.combined);
 
+        //sets colorvalue before drawing
+        batch.setColor(colorValue, colorValue, colorValue, 1);
+
         //begin draw
         batch.begin();
-        batch.draw(loadingLogo, 0, 0, epochsGame.worldWidth, epochsGame.worldHeight);
+        batch.draw(currentBackground, 0, 0, epochsGame.worldWidth, epochsGame.worldHeight);
         batch.end();
+
+        //reset color value
+        batch.setColor(1, 1,1, 1);
+
     }
 
     @Override
@@ -98,6 +173,6 @@ public class SplashScreen implements Screen {
 
     @Override
     public void dispose() {
-        mainBgsLogos.dispose();
+
     }
 }
